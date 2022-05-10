79a362d4ff4e91ea120262e0cbdf26790d201a3b
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-11 01:05:31 +0600

    getting url from input done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello dev!')
+const DownloadForm = document.getElementById('download-form');
+DownloadForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const url = e.target.url.value;
+    if (url.indexOf('https://www.youtube.com/watch') !== 0) {
+        console.log('invalid url')
+    }
+    else {
+        console.log(url)
+    }
+})
