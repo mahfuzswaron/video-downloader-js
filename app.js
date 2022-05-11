ed9f85bfafad81c41e3d492282dddaf23583121c
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-11 20:08:08 +0600

    functionality completed

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const DownloadForm = document.getElementById('download-form');
 DownloadForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const url = e.target.url.value;
-    if (url.indexOf('https://www.youtube.com/watch') !== 0) {
-        console.log('invalid url')
-    }
-    else {
-        console.log(url)
-    }
-})
+    format = '480';
+    download(url, format);
+    document.getElementById('url-input').value = '';
+});
+const download = (url, format) => {
+    const button = document.getElementById('download-button');
+    button.style.padding = '0';
+    button.innerHTML = `<iframe style='height: 52px; width:100%; border: none; overflow: hidden;' src="https://loader.to/api/button/?url=${url}&f=${format}"></iframe>`;
+    
+}
